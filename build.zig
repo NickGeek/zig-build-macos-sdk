879827c6e748bb9c23991b38134319ef6dae6a43
Nick Webster <nick.webster@trademe.co.nz>
2026-06-02 11:16:54 +1200

    Zig 0.16

--- build.zig
+++ build.zig
@@ -12,16 +12,16 @@ pub fn build(b: *std.Build) void {
             .optimize = optimize,
         }),
     });
-    lib.addCSourceFile(.{ .file = b.path("stub.c") });
-    lib.linkLibC();
+    lib.root_module.addCSourceFile(.{ .file = b.path("stub.c") });
+    lib.root_module.link_libc = true;
     addPaths(lib);
     b.installArtifact(lib);
 }
 
 pub fn addPaths(step: *std.Build.Step.Compile) void {
-    step.addSystemFrameworkPath(.{ .cwd_relative = sdkPath("/Frameworks") });
-    step.addSystemIncludePath(.{ .cwd_relative = sdkPath("/include") });
-    step.addLibraryPath(.{ .cwd_relative = sdkPath("/lib") });
+    step.root_module.addSystemFrameworkPath(.{ .cwd_relative = sdkPath("/Frameworks") });
+    step.root_module.addSystemIncludePath(.{ .cwd_relative = sdkPath("/include") });
+    step.root_module.addLibraryPath(.{ .cwd_relative = sdkPath("/lib") });
 }
 
 pub fn addPathsModule(m: *std.Build.Module) void {
